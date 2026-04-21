774191ef4cbf5f6ee547002f91035ba5db8aa77c
DouglasVolcato <douglasvolcato@gmail.com>
2026-04-21 19:36:25 -0300

    feat: add send task usecase

---
+++ services/api_gateway/internal/usecases/send_task_to_process_usecase.go
@@ -0,0 +1,32 @@
+package usecases
+
+import "github.com/douglasvolcato/binary-code-processer/api_gateway/internal/entities"
+
+type SendTaskToProcessUseCase struct {
+	Repo TaskProcessorInterface
+}
+
+func NewSendTaskToProcessUseCase(repo TaskProcessorInterface) *SendTaskToProcessUseCase {
+	return &SendTaskToProcessUseCase{
+		Repo: repo,
+	}
+}
+
+type SendTaskToProcessInput struct {
+	Messages []string
+}
+
+type SendTaskToProcessOutput struct {
+	Success bool
+	Tasks   []entities.Task
+}
+
+func (u *SendTaskToProcessUseCase) Execute(input *SendTaskToProcessInput) (*SendTaskToProcessOutput, error) {
+	err := u.Repo.SendTaskToProcess(input.Messages)
+	if err != nil {
+		return nil, err
+	}
+	return &SendTaskToProcessOutput{
+		Success: true,
+	}, nil
+}
